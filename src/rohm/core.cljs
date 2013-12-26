fa0f34155dacb91c6c7e9a36b3f2f6c0302b2005
Harry Binnendijk <harry@empanda.net>
2013-12-26 13:52:03 +1100

    add effect-queue, effect-messages and service-fn

--- src/rohm/core.cljs
+++ src/rohm/core.cljs
@@ -5,6 +5,7 @@
 
 ;; Pedestal style input queue
 (def ^:private input-queue (chan))
+(def ^:private effect-queue (chan))
 
 ;; Pedestal style route resolver
 (defn- de-route [type topic routes]
@@ -13,32 +14,45 @@
 
 ;; Pedestal style route resolver
 (defn- handle-mesg [{:keys [type topic] :as mesg} app-state routes]
-  (let [old-value (get-in @app-state topic)]
+  (let [old-value (if topic (get-in @app-state topic))]
     (if-let [func (de-route type topic routes)]
       (let [new-value (func old-value mesg)]
+        ;(.info js/console (pr-str "handle-mesg topic " topic new-value))
         (swap! app-state update-in topic (fn [_ nv] nv) new-value)
         )
       (.warn js/console (pr-str "handle-mesg: no routing for " type topic)))))
 
-;; called from the web-app in the will-mount function of the overall app
+(defn- handle-effect-mesg [{:keys [type topic] :as mesg} service]
+  (service mesg input-queue)
+  )
+
 (defn handle-messages 
   "Call this in your app's will-mount function with your global state (atom) and routes table (Pedestal
-  style vector-of-vectors)."
-  [app-state routes]
+  style vector-of-vectors).
+  Optionally takes a service-fn that receives a message from the effect queue.
+  service-fn can do remote services calls and put results with put-msg"
+  [app-state routes & [service]]
   (go (while true
-        (handle-mesg (<! input-queue) app-state routes))))
+        (handle-mesg (<! input-queue) app-state routes)))
+  (when service
+    (go (while true
+          (handle-effect-mesg (<! effect-queue) service)))))
 
 (defn put-msg 
   "Puts a Pedestal style message onto the input-queue.
   The type is a keyword and the topic is resolved as the path part of the cursor."
-  [type cursor & opts]
-  ;(.info js/console (pr-str "put-msg " type cursor opts))
-  (put! input-queue (merge {:type type :topic (:om.core/path (meta cursor))} opts)))
+  [type cursor-or-topic & opts]
+  (put! input-queue (merge {:type type :topic (or (:om.core/path (meta cursor-or-topic)) cursor-or-topic)} opts)))
 
 (defn extract-refs 
   "Helper to extract all the refs of the owner into a map of their keywordized names and values."
   [owner]
   (let [ks (keys (js->clj (.-refs owner)))
         m (into {} (map #(vector (keyword %) (.-value (om/get-node owner %))) ks))]
-    ;(.info js/console (pr-str "handle-submit " ks m))
     m))
+
+(defn effect-messages 
+  "Pedestal style: pass effect messages produced by effect functions to the effect-queue"
+  [messages]
+  (doseq [mesg messages]
+    (put! effect-queue mesg)))
