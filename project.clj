e12330eb5a08ba1d5507d3b702fc5164c5094025
Harry Binnendijk <harry@empanda.net>
2014-02-25 13:47:01 +1100

    om 0.5.0

--- project.clj
+++ project.clj
@@ -6,13 +6,13 @@
   :source-paths  ["src"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138" :scope "provided"]
+                 [org.clojure/clojurescript "0.0-2156" :scope "provided"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha" :scope "provided"]
-                 [om "0.3.0"]
+                 [om "0.5.0"]
                  [craygo/sablono "0.1.5-SNAPSHOT"]
                  ]
 
-  :plugins [[lein-cljsbuild "1.0.1"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
 
   :cljsbuild { 
               :builds [{:id "dev"
